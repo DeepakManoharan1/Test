3a19060f23b7a810b4b5b54b3db19cd50260f79d
DeepakManoharan1 <146086518+DeepakManoharan1@users.noreply.github.com>
2023-11-22 16:15:54 +0530

    with screen shot

--- smartHospital/src/test/java/runner/Runner.java
+++ smartHospital/src/test/java/runner/Runner.java
@@ -1,20 +1,17 @@
 package runner;
 
 import org.junit.runner.RunWith;
-import org.testng.annotations.DataProvider;
 
-//import io.cucumber.junit.Cucumber;
-//import io.cucumber.junit.CucumberOptions;
-//import io.cucumber.junit.CucumberOptions;
-import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.junit.Cucumber;
 //import io.cucumber.testng.CucumberOptions;
-import io.cucumber.testng.CucumberOptions;
+//import io.cucumber.testng.CucumberOptions;
+import io.cucumber.junit.CucumberOptions;
  
-//@RunWith(Cucumber.class)
-@CucumberOptions(tags="",features="src/test/resources/Features",glue ="com.stepdefinition",plugin= {"json:target/cucumber/cucumber-report.json","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},dryRun = false)
-public class Runner extends AbstractTestNGCucumberTests  {
+@RunWith(Cucumber.class)
+@CucumberOptions(tags="@FullBilling",features="src/test/resources/Features/Billing.feature",glue ="com.stepdefinition",plugin= {"json:target/cucumber/cucumber-report.json","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},dryRun = false)
+public class Runner   {
 //	@Override
-	
+//	extends AbstractTestNGCucumberTests
 //	@DataProvider(parallel=true)
 //	public Object[][] scenarios(){
 //		return super.scenarios();
